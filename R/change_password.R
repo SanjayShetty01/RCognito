02dbf8ecedc3fa1e24c03e34c4cf65d425058e8b
shettysanjay01 <shettysanjay01@gmail.com>
2024-11-01 21:26:34 +0530

    asdding base function and creating documntation

--- R/change_password.R
+++ R/change_password.R
@@ -1,3 +1,14 @@
+#' Change User Password
+#'
+#'@import paws
+#'
+#' @param old_password A character string representing the user's current password.
+#' @param new_password A character string representing the user's new password.
+#' @param token A character string representing the user's access token.
+#'
+#' @return A message indicating the success of the password change, or an error if it fails.
+#' @export
+
 change_password <- function(old_password, new_password, token) {
 
   tryCatch({
@@ -8,7 +19,10 @@ change_password <- function(old_password, new_password, token) {
                             AccessToken = token
     )
 
+    message("Password changed successfully.")
+    invisible(NULL)
+
   }, error = function(e) {
-    stop(e)
+    stop("Failed to change password: ", e$message)
   })
 }
--- R/sign_in.R
+++ R/sign_in.R
@@ -1,20 +1,31 @@
-library(paws)
+#' Sign in to AWS Cognito
+#'
+#'@import paws
+#'
+#' @param client_id A character string representing the app client ID.
+#' @param username A character string for the user's username.
+#' @param password A character string for the user's password.
+#' @param authflow A character string specifying the authentication flow (default: "USER_PASSWORD_AUTH").
+#'
+#' @return A list with user information and tokens if successful; throws an error otherwise.
+#' @export
 
-sign_in <- function(client_id, username, password, authflow) {
+sign_in <- function(client_id, username, password, authflow = "USER_PASSWORD_AUTH") {
   tryCatch({
-    cognito <-  paws::cognitoidentityprovider()
 
-    auth_parameter <-  list(USERNAME = username,
-                            PASSWORD = password)
+    cognito <- paws::cognitoidentityprovider()
 
-    user_info <- cognito$initiate_auth(AuthFlow = "USER_PASSWORD_AUTH",
-                                       AuthParameters = auth_parameter,
-                                       ClientId = client_id)
+    auth_parameter <- list(USERNAME = username, PASSWORD = password)
+
+    user_info <- cognito$initiate_auth(
+      AuthFlow = authflow,
+      AuthParameters = auth_parameter,
+      ClientId = client_id
+    )
 
     return(user_info)
 
   }, error = function(e) {
-    stop(e)
-  }
-  )
+    stop("Failed to sign in: ", e$message)
+  })
 }
