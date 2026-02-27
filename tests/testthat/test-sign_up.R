bd739cc2d1bcd34ef94557907cecc7fc8453b02e
SanjayShetty01 <shettysanjay01@gmail.com>
2026-02-27 10:37:42 +0530

    adding new test_case for error in sign_up

--- tests/testthat/test-sign_up.R
+++ tests/testthat/test-sign_up.R
@@ -30,3 +30,23 @@ testthat::expect_equal(result$CodeDeliveryDetails$AttributeName, "email")
 testthat::expect_equal(result$CodeDeliveryDetails$DeliveryMedium, "EMAIL")
 })
 
+testthat::test_that("sign_up_user handles failure with mocked AWS Cognito response", {
+  mock_cognito <- mockery::mock(list(
+    sign_up = function(ClientId, Username, Password, UserAttributes) {
+      stop("UsernameExistsException: An account with the given email already exists.")
+    }
+  ))
+
+  mockery::stub(sign_up_user, "paws::cognitoidentityprovider", mock_cognito)
+
+  testthat::expect_error(
+    sign_up_user(
+      client_id = "mock_client_id",
+      email = "existing@example.com",
+      username = "existinguser",
+      password = "mock_password123"
+    ),
+    "Failed to sign up: UsernameExistsException: An account with the given email already exists."
+  )
+})
+
